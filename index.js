ab0a2c6c1919b8cc9fc6910abe6016974484d04b
Jason DiLoreto <jasondiloreto@yahoo.com>
2020-09-30 00:12:03 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // write your code below!
+
+function happyHolidays(string) {
+  console.log("Happy holidays!")
+  return ("Happy holidays!")
+}
+
+var namespace = jason
+
+function happyHolidaysTo(string) {
+  console.log(`Happy holidays, everyone!`)
+  return (`Happy holidays, everyone!`)
+}
